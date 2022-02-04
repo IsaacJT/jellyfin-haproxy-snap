df5a1497e823f136f4cec3782b0ca23626f4b8c5
Isaac True <isaac.true@canonical.com>
2022-02-04 16:27:00 +0000

    Added install hook and configuration files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # jellyfin-haproxy-snap
 
-Reverse proxy setup for Jellyfin using HAProxy
+Reverse proxy setup for Jellyfin using HAProxy.
+
+Relies on the [itrue-jellyfin snap](https://github.com/IsaacJT/jellyfin-snap)
 
 [![Get it from the Snap Store](https://snapcraft.io/static/images/badges/en/snap-store-black.svg)](https://snapcraft.io/itrue-jellyfin-haproxy)
--- haproxy.sh
+++ haproxy.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-exec ${SNAP}/usr/bin/haproxy -f ${SNAP_COMMON}/haproxy.cfg
+SSL_CERT="${SNAP_COMMON}/ssl.crt"
+
+if [ -e "${SSL_CERT}" ]; then
+        CFG_FILE="haproxy-ssl.cfg"
+        echo "${SSL_CERT} found; loading ${CFG_FILE}"
+else
+        CFG_FILE="haproxy.cfg"
+        echo "${SSL_CERT} not found; loading ${CFG_FILE}"
+fi
+
+# Run in the foreground with the given configuration
+exec ${SNAP}/usr/sbin/haproxy -db -C ${SNAP_COMMON} -f "${CFG_FILE}"
