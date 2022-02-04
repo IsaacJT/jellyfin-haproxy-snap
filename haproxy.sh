e4a3ae859224e86c7b26a8f865d1920f7e820ab2
Isaac True <isaac.true@canonical.com>
2022-02-04 16:30:39 +0000

    Use environment variables in the haproxy config

--- haproxy.sh
+++ haproxy.sh
@@ -3,12 +3,12 @@
 SSL_CERT="${SNAP_COMMON}/ssl.crt"
 
 if [ -e "${SSL_CERT}" ]; then
-        CFG_FILE="haproxy-ssl.cfg"
+        CFG_FILE="${SNAP_COMMON}/haproxy-ssl.cfg"
         echo "${SSL_CERT} found; loading ${CFG_FILE}"
 else
-        CFG_FILE="haproxy.cfg"
+        CFG_FILE="${SNAP_COMMON}/haproxy.cfg"
         echo "${SSL_CERT} not found; loading ${CFG_FILE}"
 fi
 
 # Run in the foreground with the given configuration
-exec ${SNAP}/usr/sbin/haproxy -db -C ${SNAP_COMMON} -f "${CFG_FILE}"
+exec ${SNAP}/usr/sbin/haproxy -db -f "${CFG_FILE}"
